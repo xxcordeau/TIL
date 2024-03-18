5716163645b1186f90f6ac681a7f95f8d8dc6185
xxcordeau <qazseeszaq3219@gmail.com>
2024-03-18 21:30:00 +0000

    백준 2557번 Hello World 풀이 추가

---
+++ baekjoon/solutions/2024-03-18-2557-hello-world.js
@@ -0,0 +1,15 @@
+// https://www.acmicpc.net/problem/2557
+// Hello World
+//
+// 입력도 없이 그냥 고정 문자열 하나 출력하면 끝나는, 언어 입문용
+// 첫 문제.
+
+function solve() {
+  return 'Hello World!';
+}
+
+module.exports = solve;
+
+if (require.main === module) {
+  console.log(solve());
+}
