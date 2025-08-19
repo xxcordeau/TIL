2a83bdc922e5ae7301a9d59279dc9b65c189d22b
xxcordeau <xxcordeau@users.noreply.github.com>
2025-08-19 21:30:00 +0000

    백준 2799번 블라인드 풀이 추가

---
+++ baekjoon/solutions/2025-08-19-553-dp-1d-553.js
@@ -0,0 +1,23 @@
+// https://www.acmicpc.net/problem/2799
+// 블라인드
+//
+// n번째 상태가 바로 이전 몇 개의 상태로부터 결정되는 전형적인
+// 1차원 DP 문제. dp 배열에 작은 경우부터 차례로 채워나가면
+// 마지막 값이 곧 답이 된다.
+
+function solve(lines) {
+  const n = parseInt(lines[0].trim(), 10);
+  const dp = new Array(n + 1).fill(0);
+  if (n >= 1) dp[1] = 1;
+  for (let i = 2; i <= n; i++) {
+    dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000;
+  }
+  return String(dp[n]);
+}
+
+module.exports = solve;
+
+if (require.main === module) {
+  const input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+  console.log(solve(input));
+}
