b24385960cb7815e0084dfe96d0818117d62e0e1
xxcordeau <xxcordeau@users.noreply.github.com>
2025-11-01 21:30:00 +0000

    백준 3096번 영화제 풀이 추가

---
+++ baekjoon/solutions/2025-11-01-627-backtracking-627.js
@@ -0,0 +1,39 @@
+// https://www.acmicpc.net/problem/3096
+// 영화제
+//
+// 가능한 모든 경우를 순서대로 만들어보되, 조건에 어긋나는
+// 경로는 더 깊이 들어가지 않고 가지치기하는 백트래킹으로
+// 풀었다. 선택 - 재귀 - 취소의 흐름을 그대로 구현했다.
+
+function solve(lines) {
+  const [n, m] = lines[0].trim().split(/\s+/).map(Number);
+  const used = new Array(n + 1).fill(false);
+  const path = [];
+  const results = [];
+
+  function backtrack() {
+    if (path.length === m) {
+      results.push(path.join(' '));
+      return;
+    }
+    for (let i = 1; i <= n; i++) {
+      if (!used[i]) {
+        used[i] = true;
+        path.push(i);
+        backtrack();
+        path.pop();
+        used[i] = false;
+      }
+    }
+  }
+
+  backtrack();
+  return results.join('\n');
+}
+
+module.exports = solve;
+
+if (require.main === module) {
+  const input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+  console.log(solve(input));
+}
